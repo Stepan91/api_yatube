7fee17953d03cd1168ca6916af7781e07b024e08
Stepan Pryn <prynstepan@yandex.ru>
2020-11-21 17:19:42 +0300

    удалил лишние импорты и комменты

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,8 @@
-from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import permissions
-from django.shortcuts import get_object_or_404
 from .permissions import IsOwnerOrReadOnly
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework.response import Response
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -20,7 +17,6 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    #queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated,
                           IsOwnerOrReadOnly]
@@ -30,7 +26,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         post_id=self.kwargs['post_id']
         return Comment.objects.filter(post__id=post_id)
 
-    # как тут в таком случае коммент привязывается к посту,
+    # как тут в таком случае коммент привязывается к посту, 
     # если я удалил post.comment = serializer.validated_data?
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
