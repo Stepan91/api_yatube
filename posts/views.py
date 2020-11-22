067b2e06ac4964a5296cdd5834cfc01353550796
Stepan Pryn <prynstepan@yandex.ru>
2020-11-22 19:25:30 +0300

    4th attempt

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@ from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -18,15 +20,23 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated,
-                          IsOwnerOrReadOnly]
+    permission_classes = (permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly)
 
 
-    def get_queryset(self):
+    def get_queryset(self, *args, **kwargs):
         post_id=self.kwargs['post_id']
-        return Comment.objects.filter(post__id=post_id)
+        post = Post.objects.get(id=post_id)
+        if post:
+            return post.comments
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    # как тут в таком случае коммент привязывается к посту, 
-    # если я удалил post.comment = serializer.validated_data?
+    # а зачем здесь проверка на наличие поста,
+    # ведь мы это сделали при определении queryset,
+    # который распространяется на все методы класса?
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        post_id=self.kwargs['post_id']
+        post = Post.objects.get(id=post_id)
+        if post:
+            serializer.save(author=self.request.user)
+        return Response(status=status.HTTP_404_NOT_FOUND)
